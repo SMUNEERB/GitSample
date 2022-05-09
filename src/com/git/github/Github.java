66eb98f9aca30d4ba8b6e5c226dc13f55986e920
SMUNEERB <muneerbashask7032@gmail.com>
2022-05-09 11:00:34 +0530

    emp2 changes

--- src/com/git/github/Github.java
+++ src/com/git/github/Github.java
@@ -3,7 +3,7 @@ package com.git.github;
 public class Github {
 	public static void main(String[] args) {
 		System.out.println("Github");
-		System.out.println("add");
+		System.out.println("emp2 change");
 	}
 
 }
